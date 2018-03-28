8f8dd250d4053b1ad8147b1e80f6619c6242ad59
agray <agray@pch.com>
2018-03-28 16:06:39 -0400

    pisano

--- FibonacciNumberAgain/fibonacci.ex
+++ FibonacciNumberAgain/fibonacci.ex
@@ -5,10 +5,24 @@ defmodule Helper do
   @doc """
   """
   def read_input do
+    IO.gets("") |> String.split(" ")
+  end
+
+  @doc """
+  Compute teh Nth fibonacci number
+  """
+  def fib_number(n) do
+  end
+
+  @doc """
+  Compute the period of fib_number(n) % m
+  This is known as the Pisano period.
+  """
+  def p(m) do
   end
 
   @doc """
   """
-  def fib_number do
+  def fib_n_mod_m([n, m]) do
   end
 end
